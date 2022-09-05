397d783c09b162f41212ca4d6fa31541ff0d9d4d
P303\May07 <catbinhle@gmail.com>
2022-09-05 19:49:20 +0700

    Init Demo source code

---
+++ App.js
@@ -0,0 +1,58 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Image, View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Image style={{
+        width: '100%', 
+        height: '100%', 
+        resizeMode: 'cover',
+      }} 
+        source={require('./assets/welcomes/bg.png')}/>
+
+      <View style={{
+        width: '100%', 
+        height: '100%',  
+        position: 'absolute',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <Image style={{
+          width: 240, 
+          height: 240, 
+          resizeMode: 'contain', 
+          flex: 1
+        }} 
+          source={require('./assets/welcomes/welcome1.png')}/>
+        <View style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'flex-start'
+        }}>
+          <Text style={{
+            fontWeight:'800',
+            fontSize: 18,
+          }}>Best Service</Text>
+          <Text style={{
+            fontSize: 14,
+            textAlign: 'center',
+            marginTop: 16
+          }}>
+            We promise to provide the best services when it comes to customer experiences.
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
