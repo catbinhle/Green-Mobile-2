95ca3a8978e8e92c03c67f1dfdc42c5a29d79b77
khangquan <kedoquan@gmail.com>
2022-09-05 21:15:21 +0700

    test

--- App.js
+++ App.js
@@ -1,44 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-  // Binh comment
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Image style={styles.backgroundImg} 
-        source={require('./assets/welcomes/bg.png')}/>
+      <Image
+        style={styles.backgroundImg}
+        source={require('./assets/welcomes/bg.png')}
+      />
 
       <View style={styles.centerObj}>
-        <Image style={styles.centerImg} 
-          source={require('./assets/welcomes/welcome1.png')}/>
-        <View style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'flex-start'
-        }}>
-          <Text style={{
-            fontWeight:'800',
-            fontSize: 18,
-          }}>Best Service</Text>
-          <Text style={{
-            fontSize: 14,
-            textAlign: 'center',
-            marginTop: 16
-          }}>
-            We promise to provide the best services when it comes to customer experiences.
-          </Text>
-          <Text style={{
-            fontSize: 14,
-            textAlign: 'center',
-            marginTop: 16
-          }}>
-            version 1.0
-          </Text>
-        </View>
+        <Image
+          style={styles.centerImg}
+          source={require('./assets/welcomes/welcome1.png')}
+        />
+
+        <Text style={styles.titleStyle}>Best Service</Text>
+
+        <Text style={styles.textStyle}>
+          We promise to provide the best services when it comes to customer
+          experiences.
+        </Text>
       </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -47,30 +33,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   backgroundImg: {
-    width: '100%', 
-    height: '100%', 
+    width: '100%',
+    height: '100%',
     resizeMode: 'stretch',
   },
   centerImg: {
-    width: 240, 
-    height: 240, 
-    resizeMode: 'contain', 
+    width: 240,
+    height: 240,
+    resizeMode: 'contain',
   },
   centerObj: {
-    width: '100%', 
-    height: '100%',  
+    width: '100%',
+    height: '100%',
     position: 'absolute',
     alignItems: 'center',
     justifyContent: 'center',
   },
   titleStyle: {
-    fontWeight:'800',
+    fontWeight: '800',
     fontSize: 20,
-    marginVertical:15,
+    marginVertical: 15,
   },
   textStyle: {
     fontSize: 16,
     textAlign: 'center',
-    marginHorizontal:15,
+    marginHorizontal: 15,
   },
-});
+})
