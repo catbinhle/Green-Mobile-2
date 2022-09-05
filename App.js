451d286014e0cdb93fb833e89a4f653600e88a4d
khangquan <kedoquan@gmail.com>
2022-09-05 20:28:18 +0700

    rút gọn code

--- App.js
+++ App.js
@@ -5,44 +5,18 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Image style={{
-        width: '100%', 
-        height: '100%', 
-        resizeMode: 'cover',
-      }} 
+      <Image style={styles.backgroundImg} 
         source={require('./assets/welcomes/bg.png')}/>
 
-      <View style={{
-        width: '100%', 
-        height: '100%',  
-        position: 'absolute',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-        <Image style={{
-          width: 240, 
-          height: 240, 
-          resizeMode: 'contain', 
-          flex: 1
-        }} 
+      <View style={styles.centerObj}>
+        <Image style={styles.centerImg} 
           source={require('./assets/welcomes/welcome1.png')}/>
-        <View style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'flex-start'
-        }}>
-          <Text style={{
-            fontWeight:'800',
-            fontSize: 18,
-          }}>Best Service</Text>
-          <Text style={{
-            fontSize: 14,
-            textAlign: 'center',
-            marginTop: 16
-          }}>
-            We promise to provide the best services when it comes to customer experiences.
-          </Text>
-        </View>
+
+          <Text style={styles.titleStyle}>Best Service</Text>
+
+          <Text style={styles.textStyle}>
+            We promise to provide the best services when it comes to customer experiences.</Text>
+          
       </View>
     </View>
   );
@@ -52,7 +26,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
+  },
+  backgroundImg: {
+    width: '100%', 
+    height: '100%', 
+    resizeMode: 'stretch',
+  },
+  centerImg: {
+    width: 240, 
+    height: 240, 
+    resizeMode: 'contain', 
+  },
+  centerObj: {
+    width: '100%', 
+    height: '100%',  
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleStyle: {
+    fontWeight:'800',
+    fontSize: 20,
+    marginVertical:15,
+  },
+  textStyle: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginHorizontal:15,
   },
 });
