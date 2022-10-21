3494b736babb2853cb614a03be61de53cbd2e171
Cat Binh Le <binh.le@siliconprime.com>
2022-10-21 18:52:08 +0700

    Add axios

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,18 @@
-// import { combineReducers } from 'redux';
+import { combineReducers } from 'redux';
 
 import appReducer from './AppReducer';
 import homeReducer from './HomeReducers';
 
 // **** Notes: nếu dùng createStore của redux
-// const rootReducer = combineReducers({
-//     app: appReducer,
-//     home: homeReducer
-// })
-
-// **** Notes: nếu dùng @reduxjs/toolkit
-const rootReducer = {
+const rootReducer = combineReducers({
     app: appReducer,
     home: homeReducer
-}
+})
+
+// **** Notes: nếu dùng @reduxjs/toolkit
+// const rootReducer = {
+//     app: appReducer,
+//     home: homeReducer
+// }
 
 export default rootReducer
